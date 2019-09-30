fb9e79ccf9ad41a517c5baddc9a74e0168d996ce
Jonathan Beliën <jbe@geo6.be>
2019-09-30 10:00:51 +0200

    Add language files

---
+++ data/languages/fr/default.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    // Login
+    'Login' => 'Identifiant',
+    'Password' => 'Mot de passe',
+    'Language' => 'Langue',
+    'Forgot your password ?' => 'Mot de passe oublié ?',
+    'Log In' => 'Se connecter',
+    'The login or password you\'ve entered is incorrect.' => 'Le login ou mot de passe entré est incorrect. ',
+    // Password
+    'Account recovery' => 'Retrouvez votre compte',
+    'If you have forgotten your password, you can regain access to your account using our account recovery tool.'
+    => 'Si vous avez perdu votre mot de passe, vous pouvez récupérez l\'accès à votre compte en utilisant notre outil de récupération de compte.',
+    'This email address is not registered in our system.' => 'Cette adresse e-mail n\'est pas enregistrée dans notre système.',
+    'Your email address' => 'Votre adresse e-mail',
+    'If your email address is registered in our system we will send you an email to reset your password.'
+    => 'Si votre adresse est enregistrée dans notre système nous vous enverrons un e-mail pour réinitialiser votre mot de passe.',
+    'Next' => 'Suivant',
+    // Profile
+    'Profile' => 'Profil utilisateur',
+    'Data' => 'Vos données',
+    'Roles' => 'Vos rôles',
+    'Log out' => 'Se déconnecter',
+];
---
+++ data/languages/nl/default.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Login' => 'Gebruikersnaam',
+    'Password' => 'Wachtwoord',
+    'Language' => 'Taalkeuze',
+    'Forgot your password ?' => 'Wachtwoord vergeten ?',
+    'Log In' => 'Aanmelden',
+    'The login or password you\'ve entered is incorrect.' => 'Het gebruikersnaam of wachtwoord dat je hebt ingegeven is fout.',
+];
