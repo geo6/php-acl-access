d3e2486558240809ceb3da8f74250e4aed52cef9
jbelien <dev@jbelien.be>
2019-09-30 08:01:08 +0000

    Apply fixes from StyleCI

--- data/languages/nl/default.php
+++ data/languages/nl/default.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'Login' => 'Gebruikersnaam',
-    'Password' => 'Wachtwoord',
-    'Language' => 'Taalkeuze',
-    'Forgot your password ?' => 'Wachtwoord vergeten ?',
-    'Log In' => 'Aanmelden',
+    'Login'                                               => 'Gebruikersnaam',
+    'Password'                                            => 'Wachtwoord',
+    'Language'                                            => 'Taalkeuze',
+    'Forgot your password ?'                              => 'Wachtwoord vergeten ?',
+    'Log In'                                              => 'Aanmelden',
     'The login or password you\'ve entered is incorrect.' => 'Het gebruikersnaam of wachtwoord dat je hebt ingegeven is fout.',
 ];
