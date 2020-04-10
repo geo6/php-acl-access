f3f0a2a3b2901bebbbb74fc948a9dbb12943f1c9
Jonathan Beliën <jbe@geo6.be>
2020-04-10 15:50:10 +0200

    Split resources home/app in access management

--- resources/javascript/admin/resources.ts
+++ resources/javascript/admin/resources.ts
@@ -6,6 +6,7 @@ import $ from "jquery";
 
 import "bootstrap/js/dist/dropdown";
 import "bootstrap/js/dist/modal";
+import "bootstrap/js/dist/tab";
 import "bootstrap/js/dist/util";
 
 import getResource from "./api/get";
